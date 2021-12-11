bfb6cb0273c411205a51cc365686ee53a94c66a3
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2021-12-11 19:43:35 +0100

    Implement MMU Mappings

--- m1/src/arch/mmu.rs
+++ m1/src/arch/mmu.rs
@@ -1,26 +1,61 @@
 extern crate alloc;
-use alloc::boxed::Box;
+use alloc::vec;
+use alloc::{boxed::Box, vec::Vec};
+use core::ops::{Deref, DerefMut};
 use cortex_a::{
     asm::barrier,
     registers::{MAIR_EL1, SCTLR_EL1, TCR_EL1, TTBR0_EL1, TTBR1_EL1},
 };
 use tock_registers::interfaces::Writeable;
 
+const VA_MASK: u64 = (1 << 48) - (1 << 14);
+const PA_MASK: u64 = (1 << 48) - (1 << 14);
+const PAGE_SIZE: usize = 1 << 14;
+
 static mut MMU: MemoryManagementUnit = MemoryManagementUnit::new();
 
+#[derive(Debug)]
+pub enum Error {
+    OverlapsExistingMapping,
+}
+
+#[derive(Clone, Copy, Debug)]
 pub enum Attributes {
     Normal = 0,
     DevicenGnRnE = 1,
     DevicenGnRE = 2,
 }
 
+#[derive(Clone, Copy, Debug)]
 pub enum Permissions {
     RWX = 0,
     RW = 1,
     RX = 2,
 }
 
-pub struct OutputAddress(*mut ());
+#[derive(Eq, PartialEq, Copy, Clone, Debug)]
+pub struct VirtualAddress(*const u8);
+
+impl VirtualAddress {
+    pub fn new(addr: *const u8) -> Self {
+        Self(addr)
+    }
+    pub unsafe fn offset(&self, offset: usize) -> Self {
+        Self(self.0.add(offset))
+    }
+}
+
+#[derive(Eq, PartialEq, Copy, Clone, Debug)]
+pub struct PhysicalAddress(*const u8);
+
+impl PhysicalAddress {
+    pub fn new(addr: *const u8) -> Self {
+        Self(addr)
+    }
+    pub unsafe fn offset(&self, offset: usize) -> Self {
+        Self(self.0.add(offset))
+    }
+}
 
 #[derive(Eq, PartialEq)]
 enum DescriptorType {
@@ -33,9 +68,6 @@ enum DescriptorType {
 #[repr(C)]
 struct DescriptorEntry(u64);
 
-const VA_MASK: u64 = (1 << 48) - (1 << 14);
-const PA_MASK: u64 = (1 << 48) - (1 << 14);
-
 impl DescriptorEntry {
     const VALID_BIT: u64 = 1 << 0;
     const TABLE_BIT: u64 = 1 << 1;
@@ -44,14 +76,19 @@ impl DescriptorEntry {
         Self(0)
     }
 
-    fn new_table_desc(table_addr: Box<LevelTable>) -> Self {
-        let table_addr = Box::leak(table_addr) as *mut _;
+    fn new_table_desc(level: TranslationLevel) -> Self {
+        let table = Box::new(LevelTable::new(level));
+        let table_addr = Box::leak(table) as *mut _;
         Self(Self::VALID_BIT | Self::TABLE_BIT | (table_addr as u64 & VA_MASK))
     }
 
-    fn new_page_or_block_desc(output_addr: OutputAddress, _attributes: Attributes) -> Self {
+    fn new_page_or_block_desc(
+        physical_addr: PhysicalAddress,
+        _attributes: Attributes,
+        _permissions: Permissions,
+    ) -> Self {
         // TODO(javier-varez): Fix attributes
-        Self(Self::VALID_BIT | (output_addr.0 as u64 & VA_MASK))
+        Self(Self::VALID_BIT | (physical_addr.0 as u64 & VA_MASK))
     }
 
     fn is_table(&self) -> bool {
@@ -83,6 +120,14 @@ impl DescriptorEntry {
             DescriptorType::PageOrBlock
         } else {
             DescriptorType::Table
+        }
+    }
+
+    fn pa(&self) -> Option<PhysicalAddress> {
+        if self.is_block_or_page() {
+            Some(PhysicalAddress((self.0 & PA_MASK) as *const u8))
+        } else {
+            None
         }
     }
 }
@@ -119,59 +164,130 @@ enum TranslationLevel {
 
 impl TranslationLevel {
     fn is_last(&self) -> bool {
-        return *self == TranslationLevel::Level3;
+        *self == TranslationLevel::Level3
+    }
+
+    fn supports_block_descriptors(&self) -> bool {
+        matches!(*self, TranslationLevel::Level2 | TranslationLevel::Level3)
+    }
+
+    fn table_size(&self) -> usize {
+        match *self {
+            TranslationLevel::Level0 => 2,
+            _ => 2048,
+        }
+    }
+
+    fn address_range_size(&self) -> usize {
+        match *self {
+            TranslationLevel::Level0 => 1 << 48,
+            TranslationLevel::Level1 => 1 << 47,
+            TranslationLevel::Level2 => 1 << 36,
+            TranslationLevel::Level3 => 1 << 25,
+        }
+    }
+
+    fn entry_size(&self) -> usize {
+        1 << self.offset()
+    }
+
+    fn va_mask(&self) -> usize {
+        self.address_range_size() - self.entry_size()
+    }
+
+    fn offset(&self) -> usize {
+        match *self {
+            TranslationLevel::Level0 => 47,
+            TranslationLevel::Level1 => 36,
+            TranslationLevel::Level2 => 25,
+            TranslationLevel::Level3 => 14,
+        }
+    }
+
+    fn table_index_for_addr(&self, va: VirtualAddress) -> usize {
+        let va = va.0 as usize;
+        (va & self.va_mask()) >> self.offset()
     }
 
     fn next(&self) -> Self {
-        match self {
-            &TranslationLevel::Level0 => TranslationLevel::Level1,
-            &TranslationLevel::Level1 => TranslationLevel::Level2,
-            &TranslationLevel::Level2 => TranslationLevel::Level3,
-            &TranslationLevel::Level3 => TranslationLevel::Level3,
+        match *self {
+            TranslationLevel::Level0 => TranslationLevel::Level1,
+            TranslationLevel::Level1 => TranslationLevel::Level2,
+            TranslationLevel::Level2 => TranslationLevel::Level3,
+            TranslationLevel::Level3 => TranslationLevel::Level3,
         }
     }
 }
 
 /// Levels must be aligned at least at 16KB according to the translation granule.
 /// In addition, each level must be 11 bits, that's why we have 2048 entries in a level table
-#[repr(align(0x4000))]
+#[repr(C, align(0x4000))]
 struct LevelTable {
-    table: [DescriptorEntry; 2048],
+    table: Vec<DescriptorEntry>,
+    level: TranslationLevel,
+}
+
+impl Deref for LevelTable {
+    type Target = Vec<DescriptorEntry>;
+    fn deref(&self) -> &Self::Target {
+        &self.table
+    }
+}
+
+impl DerefMut for LevelTable {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.table
+    }
 }
 
 impl LevelTable {
-    const fn new() -> Self {
+    const fn new_empty(level: TranslationLevel) -> Self {
         Self {
-            table: [INVALID_DESCRIPTOR; 2048],
+            table: Vec::new(),
+            level,
         }
+    }
+
+    fn new(level: TranslationLevel) -> Self {
+        Self {
+            table: vec![INVALID_DESCRIPTOR; level.table_size()],
+            level,
+        }
+    }
+
+    fn iter(&self) -> core::slice::Iter<'_, DescriptorEntry> {
+        self.table.iter()
     }
 }
 
 pub struct MemoryManagementUnit {
     initialized: bool,
-    level0: Option<Box<[DescriptorEntry; 2]>>,
+    level0: LevelTable,
 }
 
 impl MemoryManagementUnit {
     const fn new() -> Self {
         Self {
             initialized: false,
-            level0: None,
+            level0: LevelTable::new_empty(TranslationLevel::Level0),
         }
     }
 
     fn init(&mut self) {
-        self.level0 = Some(Box::new([INVALID_DESCRIPTOR; 2]));
+        self.level0 = LevelTable::new(TranslationLevel::Level0)
+    }
 
-        let ram_base = 0x10000000000 as *const ();
+    fn add_default_mappings(&mut self) {
+        let ram_base = 0x10000000000 as *const u8;
         let ram_size = 0x800000000;
         self.map_region(
-            ram_base,
-            ram_base,
+            VirtualAddress::new(ram_base),
+            PhysicalAddress::new(ram_base),
             ram_size,
             Attributes::Normal,
             Permissions::RWX,
-        );
+        )
+        .expect("No other mapping overlaps");
     }
 
     pub fn init_and_enable(&mut self) {
@@ -179,6 +295,7 @@ impl MemoryManagementUnit {
             return;
         }
         self.init();
+        self.add_default_mappings();
         self.enable();
         self.initialized = true;
     }
@@ -204,8 +321,8 @@ impl MemoryManagementUnit {
                 + TCR_EL1::T1SZ.val(16),
         );
 
-        TTBR0_EL1.set_baddr(self.level0.as_ref().unwrap().as_ptr() as u64);
-        TTBR1_EL1.set_baddr(self.level0.as_ref().unwrap().as_ptr() as u64);
+        TTBR0_EL1.set_baddr(self.level0.table.as_ptr() as u64);
+        TTBR1_EL1.set_baddr(self.level0.table.as_ptr() as u64);
 
         unsafe {
             barrier::dsb(barrier::ISHST);
@@ -219,42 +336,148 @@ impl MemoryManagementUnit {
         }
     }
 
-    fn map_region(
-        &mut self,
-        va: *const (),
-        pa: *const (),
+    fn map_region_internal(
+        mut va: VirtualAddress,
+        mut pa: PhysicalAddress,
         size: usize,
         attributes: Attributes,
         permissions: Permissions,
-    ) {
-        // TODO(jalv): Implement all levels
-        let translation_table = self.level0.as_mut().unwrap();
-        let mut va = (va as u64 & VA_MASK) as *const ();
-        let mut pa = (pa as u64 & PA_MASK) as *const ();
-        let entry_size = 1 << 47;
+        translation_table: &mut LevelTable,
+    ) -> Result<(), Error> {
+        let level = translation_table.level;
+        let entry_size = level.entry_size();
 
         let mut remaining_size = size;
         while remaining_size != 0 {
-            let index = va as usize >> 47;
-            match translation_table[index].ty() {
-                DescriptorType::Invalid => {
-                    translation_table[index] =
-                        DescriptorEntry::new_table_desc(Box::new(LevelTable::new()));
+            let index = translation_table.level.table_index_for_addr(va);
+            let descriptor_entry = &mut translation_table[index];
+
+            if (size >= entry_size) && level.supports_block_descriptors() {
+                // We could allocate a block or page descriptor here. Else we would need a next
+                // level table
+                match descriptor_entry.ty() {
+                    DescriptorType::Invalid => {
+                        *descriptor_entry =
+                            DescriptorEntry::new_page_or_block_desc(pa, attributes, permissions);
+                    }
+                    DescriptorType::Table => {
+                        // TODO(javier-varez): Handle consolidation of mappings
+                        return Err(Error::OverlapsExistingMapping);
+                    }
+                    DescriptorType::PageOrBlock
+                        if descriptor_entry.pa().expect("Desc is a page/block") != pa =>
+                    {
+                        return Err(Error::OverlapsExistingMapping);
+                    }
+                    _ => {}
                 }
-                DescriptorType::Table => {}
-                DescriptorType::PageOrBlock => {}
+            } else {
+                // Need to have a table and some granularity inside
+                if descriptor_entry.is_block_or_page() {
+                    return Err(Error::OverlapsExistingMapping);
+                }
+
+                if descriptor_entry.is_invalid() {
+                    *descriptor_entry = DescriptorEntry::new_table_desc(level.next());
+                }
+
+                Self::map_region_internal(
+                    va,
+                    pa,
+                    size,
+                    attributes,
+                    permissions,
+                    descriptor_entry.get_table().expect("Is a table"),
+                )?;
             }
 
             unsafe {
-                pa = pa.add(entry_size);
-                va = va.add(entry_size);
+                pa = pa.offset(entry_size);
+                va = va.offset(entry_size);
             }
 
             remaining_size = remaining_size.saturating_sub(entry_size);
         }
+        Ok(())
+    }
+
+    pub fn map_region(
+        &mut self,
+        va: VirtualAddress,
+        pa: PhysicalAddress,
+        mut size: usize,
+        attributes: Attributes,
+        permissions: Permissions,
+    ) -> Result<(), Error> {
+        let translation_table = &mut self.level0;
+
+        // Size needs to be aligned to page size
+        if (size % PAGE_SIZE) != 0 {
+            size = size + PAGE_SIZE - (size % PAGE_SIZE);
+        }
+
+        Self::map_region_internal(va, pa, size, attributes, permissions, translation_table)
     }
 }
 
 pub fn initialize_mmu() {
     unsafe { MMU.init_and_enable() };
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn single_page_mapping() {
+        let mut mmu = MemoryManagementUnit::new();
+        mmu.init();
+
+        assert!(mmu.level0[0].is_invalid());
+
+        let from = VirtualAddress(0x012345678000 as *const u8);
+        let to = PhysicalAddress(0x012345678000 as *const u8);
+        let size = 1 << 14;
+        mmu.map_region(from, to, size, Attributes::Normal, Permissions::RWX)
+            .expect("Adding region was successful");
+
+        let level0 = &mut mmu.level0;
+        assert_eq!(level0.level, TranslationLevel::Level0);
+        assert!(!level0[0].is_invalid());
+        assert!(level0[0].is_table());
+        assert!(level0[1].is_invalid());
+
+        let level1 = level0[0].get_table().expect("Is a table");
+        assert_eq!(level1.level, TranslationLevel::Level1);
+        for (idx, desc) in level1.iter().enumerate() {
+            if idx == 0x12 {
+                assert!(desc.is_table());
+            } else {
+                assert!(desc.is_invalid());
+            }
+        }
+
+        let level2 = level1[0x12].get_table().expect("Is a table");
+        assert_eq!(level2.level, TranslationLevel::Level2);
+
+        for (idx, desc) in level2.iter().enumerate() {
+            if idx == 0x1a2 {
+                assert!(desc.is_table());
+            } else {
+                assert!(desc.is_invalid());
+            }
+        }
+
+        let level3 = level2[0x1a2].get_table().expect("Is a table");
+        assert_eq!(level3.level, TranslationLevel::Level3);
+
+        for (idx, desc) in level3.iter().enumerate() {
+            if idx == 0x59e {
+                assert!(desc.is_block_or_page());
+                assert_eq!(desc.pa(), Some(to));
+            } else {
+                assert!(desc.is_invalid());
+            }
+        }
+    }
 }
