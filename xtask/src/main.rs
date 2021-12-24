5f38911e6667e77ba36524c27563ca01458da98e
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2021-12-24 23:51:33 +0100

    Add xtask to run qemu automatically

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -5,14 +5,23 @@ use structopt::StructOpt;
 #[derive(Debug, StructOpt)]
 #[structopt(name = "xtask", about = "runs automated tasks with cargo")]
 enum Options {
-    /// Builds fw for p1c0
-    Build {
-        /// Builds the release version of the FW
+    /// Runs Qemu with the built FW.
+    /// Make sure to use an M1 compatible qemu version like:
+    /// <https://github.com/Javier-varez/qemu-apple-m1/suites/4750576312/artifacts/131568164>
+    Run {
+        /// Use the `release` FW.
         #[structopt(long)]
         release: bool,
     },
-    /// Runs all tests
+    /// Builds FW for p1c0. Generates a `.macho` file in the p1c0 folder.
+    Build {
+        /// Builds with the `release` profile.
+        #[structopt(long)]
+        release: bool,
+    },
+    /// Runs all tests.
     Test,
+    /// Runs clippy on all sources.
     Clippy,
 }
 
@@ -42,10 +51,18 @@ fn run_clippy() -> Result<(), anyhow::Error> {
     Ok(())
 }
 
+fn run_qemu(release: bool) -> Result<(), anyhow::Error> {
+    build(release)?;
+    cmd!("qemu-system-aarch64 -machine apple-m1 -bios fw/p1c0.macho -serial stdio --display none")
+        .run()?;
+    Ok(())
+}
+
 fn main() -> Result<(), anyhow::Error> {
     let opts = Options::from_args();
 
     match opts {
+        Options::Run { release } => run_qemu(release)?,
         Options::Build { release } => build(release)?,
         Options::Test => run_tests()?,
         Options::Clippy => run_clippy()?,
