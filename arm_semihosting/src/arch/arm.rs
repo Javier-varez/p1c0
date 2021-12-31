1292d3b86173a157cdfbb0711b04969a0ddd0221
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2021-12-31 18:17:26 +0100

    Semihosting: unify error checking

--- arm_semihosting/src/arch/arm.rs
+++ arm_semihosting/src/arch/arm.rs
@@ -1,21 +1,19 @@
-use crate::{HostResult, Operation};
+use crate::Operation;
 
 use core::arch::asm;
 
 #[inline]
 #[cfg_attr(target_cpu = "arm", instruction_set(arm::a32))]
-pub(crate) fn call_host(op: &Operation) -> HostResult {
+pub(crate) unsafe fn call_host_unchecked(op: &Operation) -> isize {
     let op_code = op.code();
     let args = op.args();
     let mut result: isize;
 
-    unsafe {
-        asm!(
-            "svc #0x123456",
-            inlateout("r0") op_code => result,
-            in("r1") args,
-        )
-    }
+    asm!(
+        "svc #0x123456",
+        inlateout("r0") op_code => result,
+        in("r1") args,
+    );
 
-    HostResult(result)
+    result
 }
