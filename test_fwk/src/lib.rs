46189cb4fbebf8684e1db28f7da558b14bafdef9
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2022-01-03 19:48:27 +0100

    TestFwk: Colorize test output

--- test_fwk/src/lib.rs
+++ test_fwk/src/lib.rs
@@ -3,29 +3,31 @@
 use arm_semihosting::println;
 use core::ops::Fn;
 
+use ansi_rgb::{cyan_blue, green_cyan, red, Foreground};
+
 use core::panic::PanicInfo;
 
 pub fn runner(tests: &[&dyn Testable]) {
-    println!("Starting test execution");
+    println!("{}", "Starting test execution".fg(cyan_blue()));
     tests.iter().for_each(|test| test.run());
-    println!("Done with test execution");
+    println!("{}", "Done with test execution".fg(green_cyan()));
     arm_semihosting::exit(0);
 }
 
 pub fn runner_should_panic(tests: &[&dyn Testable]) {
-    println!("Starting test execution");
+    println!("{}", "Starting test execution".fg(cyan_blue()));
     tests.iter().for_each(|test| test.run());
-    println!("Done with test execution");
+    println!("{}", "Done with test execution".fg(red()));
     arm_semihosting::exit(1);
 }
 
 pub fn panic_handler(panic_info: &PanicInfo) -> ! {
-    println!("Panicked {:?}", panic_info);
+    println!("{} {:?}", "Panicked at:".fg(red()), panic_info);
     arm_semihosting::exit(1);
 }
 
 pub fn panic_handler_should_panic(panic_info: &PanicInfo) -> ! {
-    println!("Panicked {:?}", panic_info);
+    println!("{} {:?}", "Expected panic at:".fg(green_cyan()), panic_info);
     arm_semihosting::exit(0);
 }
 
@@ -39,8 +41,11 @@ where
 {
     fn run(&self) {
         let type_name = core::any::type_name::<Self>();
-        println!("START {}", type_name);
+        println!(
+            "{} {}",
+            "Running test:".fg(cyan_blue()),
+            type_name.fg(cyan_blue())
+        );
         self();
-        println!("END");
     }
 }
