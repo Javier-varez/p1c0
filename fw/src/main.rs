cf334174721a976970541f60de3adb85798bba7a
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2022-01-03 13:32:46 +0100

    p1c0-fw: Add on-target tests
    Run tests using the qemu emulator. You will need a specific qemu version
    that can be found here:
    https://github.com/Javier-varez/qemu-apple-m1.git

--- fw/src/main.rs
+++ fw/src/main.rs
@@ -1,65 +1,25 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(test_fwk::runner_should_panic)]
+#![reexport_test_harness_main = "test_main"]
 #![feature(default_alloc_error_handler)]
 
 use m1::println;
 
 use embedded_graphics::pixelcolor::Rgb888;
-use m1::boot_args::{get_boot_args, BootArgs};
+use m1::boot_args::get_boot_args;
 use m1::display::Display;
 use tinybmp::Bmp;
 
-const ATE_LOGO_DATA: &[u8] = include_bytes!("../ate_logo.bmp");
-
-#[panic_handler]
-fn panic_handler(panic_info: &core::panic::PanicInfo) -> ! {
-    println!("Panicked with message: {:?}", panic_info);
-
-    #[cfg(feature = "emulator")]
-    arm_semihosting::exit(1);
-
-    #[cfg(not(feature = "emulator"))]
-    loop {}
-}
-
-fn print_boot_args(boot_args: &BootArgs) {
-    println!("===== BOOT ARGS =====");
-    println!("Revision:           {}", boot_args.revision);
-    println!("Version:            {}", boot_args.version);
-    println!("Virtual base:       0x{:x}", boot_args.virt_base);
-    println!("Physical base:      0x{:x}", boot_args.phys_base);
-    println!("Mem size:           0x{:x}", boot_args.mem_size);
-    println!("Top of kernel data: 0x{:x}", boot_args.top_of_kernel_data);
-    println!("Video base:         {:?}", boot_args.boot_video.base);
-    println!("Video num displays: {}", boot_args.boot_video.display);
-    println!("Video stride:       {}", boot_args.boot_video.stride);
-    println!("Video width:        {}", boot_args.boot_video.width);
-    println!("Video height:       {}", boot_args.boot_video.height);
-    println!("Video depth:        0x{:x}", boot_args.boot_video.depth);
-    println!("Machine type:       {}", boot_args.machine_type);
-    println!("Device tree:        {:?}", boot_args.device_tree);
-    println!("Device tree size:   0x{:x}", boot_args.device_tree_size);
-    println!("Boot flags:         {}", boot_args.boot_flags);
-    println!("Mem size actual:    0x{:x}", boot_args.mem_size_actual);
-    println!();
-}
+use p1c0::print_boot_args;
 
 #[cfg(feature = "emulator")]
-fn print_semihosting_caps() {
-    let ext = arm_semihosting::load_extensions().unwrap();
+use p1c0::print_semihosting_caps;
 
-    println!("Running emulator with semihosting extensions:");
-    println!("Extended exit:          {}", ext.supports_extended_exit());
-    println!("Stdout-stderr support:  {}", ext.supports_stdout_stderr());
-    println!(
-        "Cmdline arguments: [{}]",
-        arm_semihosting::get_cmd_line().unwrap()
-    );
-    println!();
-}
+const ATE_LOGO_DATA: &[u8] = include_bytes!("../ate_logo.bmp");
 
-#[no_mangle]
-pub extern "C" fn kernel_main() -> ! {
+fn kernel_entry() {
     let logo = Bmp::<Rgb888>::from_slice(ATE_LOGO_DATA).unwrap();
     Display::init(&logo);
 
@@ -77,10 +37,47 @@ pub extern "C" fn kernel_main() -> ! {
     println!("let's cause a page fault!");
     let val = unsafe { *addr };
     println!("Hah, value is {}", val);
+}
+
+#[no_mangle]
+#[cfg(not(test))]
+pub extern "C" fn kernel_main() -> ! {
+    kernel_entry();
 
     #[cfg(feature = "emulator")]
     arm_semihosting::exit(0);
 
     #[cfg(not(feature = "emulator"))]
     loop {}
+}
+
+#[panic_handler]
+#[cfg(not(test))]
+fn panic_handler(panic_info: &core::panic::PanicInfo) -> ! {
+    println!("Panicked with message: {:?}", panic_info);
+
+    #[cfg(feature = "emulator")]
+    arm_semihosting::exit(1);
+
+    #[cfg(not(feature = "emulator"))]
+    loop {}
+}
+
+#[panic_handler]
+#[cfg(test)]
+fn panic_handler(panic_info: &core::panic::PanicInfo) -> ! {
+    test_fwk::panic_handler_should_panic(panic_info);
+}
+
+#[no_mangle]
+#[cfg(test)]
+pub extern "C" fn kernel_main() {
+    #[cfg(test)]
+    test_main();
+}
+
+#[test_case]
+fn test_kernel_entry() {
+    // Currently this is expected to panic
+    kernel_entry();
 }
