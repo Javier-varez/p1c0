ee0a078301a3d72c0c59b14a73ae9fa9e7f6a980
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2022-01-04 17:06:28 +0100

    m1: Implement Adt parsing

--- fw/src/main.rs
+++ fw/src/main.rs
@@ -5,19 +5,44 @@
 #![reexport_test_harness_main = "test_main"]
 #![feature(default_alloc_error_handler)]
 
-use m1::println;
+extern crate alloc;
+use alloc::vec::Vec;
 
 use embedded_graphics::pixelcolor::Rgb888;
-use m1::boot_args::get_boot_args;
-use m1::display::Display;
 use tinybmp::Bmp;
 
 use p1c0::print_boot_args;
+
+use m1::{
+    adt::{get_adt, Adt},
+    boot_args::get_boot_args,
+    display::Display,
+    println,
+};
 
 #[cfg(feature = "emulator")]
 use p1c0::print_semihosting_caps;
 
 const ATE_LOGO_DATA: &[u8] = include_bytes!("../ate_logo.bmp");
+
+fn print_compatible(adt: &Adt) {
+    let node = adt.find_node("/").expect("There is a root node");
+    let compat = node
+        .find_property("compatible")
+        .expect("There is a compatible prop");
+    let compatibles: Vec<_> = compat.str_list_value().collect();
+    println!("ADT Compatible: {:?}", compatibles);
+    println!()
+}
+
+fn print_uart_reg(adt: &Adt) {
+    let reg = adt
+        .find_node("/arm-io/uart0")
+        .and_then(|uart_node| uart_node.find_property("reg"))
+        .expect("The UART0 has a \"reg\" property");
+    println!("Uart reg: 0x{:x}", reg.usize_value().unwrap());
+    println!()
+}
 
 fn kernel_entry() {
     let logo = Bmp::<Rgb888>::from_slice(ATE_LOGO_DATA).unwrap();
@@ -32,6 +57,10 @@ fn kernel_entry() {
 
     #[cfg(feature = "emulator")]
     print_semihosting_caps();
+
+    let adt = get_adt().expect("Valid ADT");
+    print_compatible(&adt);
+    print_uart_reg(&adt);
 
     let addr = 0x00007FFFFFFFFFFF as *const u64;
     println!("let's cause a page fault!");
--- m1/src/lib.rs
+++ m1/src/lib.rs
@@ -1,6 +1,7 @@
 #![cfg_attr(not(test), no_std)]
 #![feature(allocator_api)]
 
+pub mod adt;
 pub mod arch;
 pub mod boot_args;
 mod collections;
