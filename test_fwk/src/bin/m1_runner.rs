e4c1d20fc49b0c8b7abc54a4484e8b143cb39613
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2022-02-01 00:53:47 +0100

    TestFwk: Add configuration in toml

--- test_fwk/src/bin/m1_runner.rs
+++ test_fwk/src/bin/m1_runner.rs
@@ -1,5 +1,8 @@
-use std::error::Error;
+use anyhow::anyhow;
+use anyhow::Context;
+use std::{error::Error, fs::File, io::Read, path::Path};
 use structopt::StructOpt;
+use toml::Value;
 use xshell::{cmd, rm_rf};
 
 #[derive(StructOpt)]
@@ -11,8 +14,75 @@ struct Opts {
     fw_elf: String,
 }
 
+#[derive(Debug, Clone)]
+struct Config {
+    show_display: bool,
+    show_stdio: bool,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            show_stdio: false,
+            show_display: false,
+        }
+    }
+}
+
+fn parse_config(manifest_path: &Path) -> anyhow::Result<Config> {
+    let cargo_toml: Value = {
+        let mut content = String::new();
+        File::open(manifest_path)
+            .context("Failed to open Cargo.toml")?
+            .read_to_string(&mut content)
+            .context("Failed to read Cargo.toml")?;
+        content
+            .parse::<Value>()
+            .context("Failed to parse Cargo.toml")?
+    };
+
+    let mut config = Config::default();
+
+    let config_toml = match cargo_toml.get("m1_runner") {
+        Some(config_toml) => config_toml
+            .as_table()
+            .ok_or_else(|| anyhow!("invalid m1_runner config found: {:?}", config_toml))?,
+        None => {
+            return Ok(config);
+        }
+    };
+
+    for (k, v) in config_toml {
+        match k.as_str() {
+            "show_display" => {
+                let val = v
+                    .as_bool()
+                    .ok_or_else(|| anyhow!("show_display should be a boolean"))?;
+                config.show_display = val;
+            }
+            "show_stdio" => {
+                let val = v
+                    .as_bool()
+                    .ok_or_else(|| anyhow!("show_stdio should be a boolean"))?;
+                config.show_stdio = val;
+            }
+            _ => {
+                return Err(anyhow!("Unexpected key found in m1_config: {:?}", k));
+            }
+        }
+    }
+
+    Ok(config)
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     let opts = Opts::from_args();
+
+    let manifest_path = std::env::var("CARGO_MANIFEST_DIR")
+        .ok()
+        .map(|dir| Path::new(&dir).join("Cargo.toml"))
+        .expect("WARNING: `CARGO_MANIFEST_DIR` env variable not set");
+    let config = parse_config(&manifest_path)?;
 
     cmd!("rust-objcopy")
         .arg("-O")
@@ -20,8 +90,23 @@ fn main() -> Result<(), Box<dyn Error>> {
         .arg(opts.fw_elf)
         .arg("_test_fw.macho")
         .run()?;
-    cmd!("qemu-system-aarch64 -machine apple-m1 -bios _test_fw.macho -serial none --display none -semihosting")
-        .run()?;
+    let qemu_cmd = cmd!("qemu-system-aarch64 -machine apple-m1 -bios _test_fw.macho -semihosting");
+
+    let mut additional_args = vec![];
+    if !config.show_display {
+        additional_args.push("--display");
+        additional_args.push("none");
+    };
+
+    additional_args.push("-serial");
+
+    if !config.show_stdio {
+        additional_args.push("none");
+    } else {
+        additional_args.push("stdio");
+    };
+
+    qemu_cmd.args(additional_args.iter()).run()?;
     rm_rf("_test_fw.macho")?;
     Ok(())
 }
