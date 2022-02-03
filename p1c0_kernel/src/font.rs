11a70e1dcfed569566f2e674198fd599a295d3ff
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2022-02-03 22:29:16 +0100

    Remove FiraCode40 font

--- p1c0_kernel/src/font.rs
+++ p1c0_kernel/src/font.rs
@@ -13,18 +13,6 @@ const CHARS_PER_ROW: u32 = 32;
 const GLYPH_MAPPING: StrGlyphMapping =
     StrGlyphMapping::new("\0 ~\0\u{00A0}ÿ", '?' as usize - ' ' as usize);
 
-/// The 40 point size with a character size of 26x50 pixels.
-pub const FIRA_CODE_40: MonoFont = MonoFont {
-    image: ImageRaw::new_binary(include_bytes!("../FiraCode40.raw"), CHARS_PER_ROW * 26),
-
-    glyph_mapping: &GLYPH_MAPPING,
-    character_size: Size::new(26, 50),
-    character_spacing: 0,
-    baseline: 6,
-    underline: DecorationDimensions::new(18, 1),
-    strikethrough: DecorationDimensions::new(13, 1),
-};
-
 /// The 30 point size with a character size of 20x38 pixels.
 pub const FIRA_CODE_30: MonoFont = MonoFont {
     image: ImageRaw::new_binary(include_bytes!("../FiraCode30.raw"), CHARS_PER_ROW * 20),
