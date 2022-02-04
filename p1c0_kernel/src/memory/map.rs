80e180ea4d7a45e86826b63f34afe6107637bfbb
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2022-02-04 16:36:09 +0100

    Implement address space abstraction

--- p1c0_kernel/src/memory/map.rs
+++ p1c0_kernel/src/memory/map.rs
@@ -1,4 +1,7 @@
-use super::address::{LogicalAddress, VirtualAddress};
+use super::{
+    address::{LogicalAddress, PhysicalAddress, VirtualAddress},
+    Permissions,
+};
 
 /// This is the base address for logical addresses.
 pub const KERNEL_LOGICAL_BASE: LogicalAddress =
@@ -7,3 +10,108 @@ pub const KERNEL_LOGICAL_SIZE: usize = 1024 * 1024 * 1024 * 1024; // 1 TB
 
 pub const ADT_VIRTUAL_BASE: VirtualAddress =
     unsafe { VirtualAddress::new_unchecked(0xFFFF000000000000 as *const u8) };
+
+extern "C" {
+    static _text_start: u8;
+    static _text_end: u8;
+    static _rodata_start: u8;
+    static _rodata_end: u8;
+    static _data_start: u8;
+    static _data_end: u8;
+    static _arena_start: u8;
+    static _arena_end: u8;
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum KernelSectionId {
+    Text,
+    RoData,
+    Data,
+    Arena,
+}
+
+pub const ALL_SECTIONS: [KernelSectionId; 4] = [
+    KernelSectionId::Text,
+    KernelSectionId::RoData,
+    KernelSectionId::Data,
+    KernelSectionId::Arena,
+];
+
+pub struct KernelSection {
+    name: &'static str,
+    start: PhysicalAddress,
+    size_bytes: usize,
+    permissions: Permissions,
+}
+
+impl KernelSection {
+    pub fn from_id(id: KernelSectionId) -> Self {
+        let (name, start, end, permissions) = unsafe {
+            match id {
+                KernelSectionId::Text => (
+                    ".text",
+                    &_text_start as *const u8,
+                    &_text_end as *const u8,
+                    Permissions::RX,
+                ),
+                KernelSectionId::RoData => (
+                    ".rodata",
+                    &_rodata_start as *const _,
+                    &_rodata_end as *const _,
+                    Permissions::RO,
+                ),
+                KernelSectionId::Data => (
+                    ".data",
+                    &_data_start as *const _,
+                    &_data_end as *const _,
+                    Permissions::RW,
+                ),
+                KernelSectionId::Arena => (
+                    ".arena",
+                    &_arena_start as *const _,
+                    &_arena_end as *const _,
+                    Permissions::RW,
+                ),
+            }
+        };
+
+        let size_bytes = unsafe { end.offset_from(start) as usize };
+        let start = if crate::arch::mmu::is_initialized() {
+            // After relocation this is a logical address.
+            LogicalAddress::try_from_ptr(start)
+                .expect("KernelSection should be logical after reloc")
+                .into_physical()
+        } else {
+            PhysicalAddress::try_from_ptr(start).expect("KernelSection should be aligned")
+        };
+
+        Self {
+            name,
+            start,
+            size_bytes,
+            permissions,
+        }
+    }
+
+    pub fn name(&self) -> &'static str {
+        self.name
+    }
+
+    pub fn pa(&self) -> PhysicalAddress {
+        self.start
+    }
+
+    pub fn la(&self) -> LogicalAddress {
+        self.start
+            .try_into_logical()
+            .expect("KernelSection should be convertible to logical address")
+    }
+
+    pub fn size_bytes(&self) -> usize {
+        self.size_bytes
+    }
+
+    pub fn permissions(&self) -> Permissions {
+        self.permissions
+    }
+}
