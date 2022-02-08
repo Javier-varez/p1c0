8b6a4121441cfd68a7b5d964a9754d0cef5901a7
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2022-02-08 23:43:51 +0100

    SpinLock should use critical sections
    This would ensure that deadlock cannot happen due to a spinlock being
    held while the exception is taken and later tried to be taken in the
    interrupt context, which would deadlock.
    
    In reality this is only a problem in the same core, so spinlocks across
    different cores would still work fine.
    
    For that to work we need to implement a per-cpu critical nesting
    variable, but since there is no smp support so far this should be fine.

--- p1c0_kernel/src/sync/spinlock.rs
+++ p1c0_kernel/src/sync/spinlock.rs
@@ -1,4 +1,8 @@
 use core::sync::atomic;
+use cortex_a::{asm::barrier, registers::DAIF};
+use tock_registers::interfaces::Writeable;
+
+static CRITICAL_NESTING: atomic::AtomicU32 = atomic::AtomicU32::new(0);
 
 use core::cell::UnsafeCell;
 
@@ -27,6 +31,18 @@ impl<T> SpinLock<T> {
             .is_err()
         {}
 
+        let prev_nesting = CRITICAL_NESTING.fetch_add(1, atomic::Ordering::Acquire);
+        if prev_nesting == core::u32::MAX {
+            panic!("We have reached the maximum value for CRITICAL_NESTING. This is MOST LIKELY a bug in user code");
+        } else if prev_nesting == 0 {
+            // Disable exceptions here because they were enabled (CRITICAL_NESTING was 0)
+            DAIF.write(DAIF::D::Masked + DAIF::I::Masked + DAIF::A::Masked + DAIF::F::Masked);
+
+            // Add a barrier here to ensure that subsequent memory accesses really execute within
+            // the critical section
+            unsafe { barrier::dsb(barrier::ISHST) };
+        }
+
         SpinLockGuard {
             lock: self,
             data: unsafe { &mut *self.data.get() },
@@ -34,6 +50,19 @@ impl<T> SpinLock<T> {
     }
 
     fn unlock(&self) {
+        let prev_nesting = CRITICAL_NESTING.fetch_sub(1, atomic::Ordering::Release);
+        if prev_nesting == core::u32::MAX {
+            panic!("We have reached the maximum value for CRITICAL_NESTING. This is MOST LIKELY a bug in user code");
+        } else if prev_nesting == 1 {
+            // Add a barrier here to ensure that memory accesses finish before enabling exceptions
+            unsafe { barrier::dsb(barrier::ISHST) };
+
+            // Enable exceptions here because they were disabled (CRITICAL_NESTING was 1)
+            DAIF.write(
+                DAIF::D::Unmasked + DAIF::I::Unmasked + DAIF::A::Unmasked + DAIF::F::Unmasked,
+            );
+        }
+
         self.lock.swap(false, atomic::Ordering::Release);
     }
 }
