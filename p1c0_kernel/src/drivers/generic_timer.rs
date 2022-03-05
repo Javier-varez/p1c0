df52b632fb4279e00b5ef9cf6ad4e90bb72e5deb
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2022-03-05 21:00:11 +0100

    Configure timer IRQ and handle its FIQ

--- p1c0_kernel/src/drivers/generic_timer.rs
+++ p1c0_kernel/src/drivers/generic_timer.rs
@@ -1,20 +1,37 @@
 use cortex_a::{
     asm::barrier,
-    registers::{CNTFRQ_EL0, CNTVCT_EL0},
+    registers::{CNTFRQ_EL0, CNTVCT_EL0, CNTV_CTL_EL0, CNTV_TVAL_EL0},
 };
-use tock_registers::interfaces::Readable;
+use tock_registers::interfaces::{Readable, Writeable};
 
-pub struct GenericTimer {}
+use core::sync::atomic::{AtomicU32, Ordering};
+
+pub struct GenericTimer {
+    ticks_per_cycle: AtomicU32,
+}
 
 impl GenericTimer {
     const fn new() -> Self {
-        Self {}
+        Self {
+            ticks_per_cycle: AtomicU32::new(0),
+        }
     }
 }
 
 impl GenericTimer {
     pub fn resolution(&self) -> u64 {
         CNTFRQ_EL0.get()
+    }
+
+    pub fn initialize(&self, interval: core::time::Duration) {
+        let ticks_per_cycle =
+            ((CNTFRQ_EL0.get() * interval.as_nanos() as u64) / 1_000_000_000) as u32;
+        crate::println!("Ticks per cycle {}", ticks_per_cycle);
+        CNTV_TVAL_EL0.set(ticks_per_cycle as u64);
+        CNTV_CTL_EL0.write(CNTV_CTL_EL0::IMASK::CLEAR + CNTV_CTL_EL0::ENABLE::SET);
+
+        self.ticks_per_cycle
+            .store(ticks_per_cycle, Ordering::Relaxed)
     }
 
     pub fn ticks(&self) -> u64 {
@@ -32,6 +49,17 @@ impl GenericTimer {
         let start = self.ticks();
 
         while self.ticks() < (start + ticks) {}
+    }
+
+    pub fn handle_irq(&self) {
+        CNTV_TVAL_EL0.set(self.ticks_per_cycle.load(Ordering::Relaxed) as u64);
+        CNTV_CTL_EL0.write(CNTV_CTL_EL0::IMASK::CLEAR + CNTV_CTL_EL0::ENABLE::SET);
+    }
+
+    pub fn is_irq_active(&self) -> bool {
+        CNTV_CTL_EL0.matches_all(
+            CNTV_CTL_EL0::IMASK::CLEAR + CNTV_CTL_EL0::ENABLE::SET + CNTV_CTL_EL0::ISTATUS::SET,
+        )
     }
 }
 
