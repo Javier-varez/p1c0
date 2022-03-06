87beceb6f75772b1ba936cce3ae2e39489c9993d
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2022-03-06 01:54:30 +0100

    Add some thread tests
    Just to verify threads get scheduled and run. Need to add separately a
    handle for the thread to allow them to borrow data that is not static.
    
    That would also clean up these tests a bit.

--- test_fwk/src/lib.rs
+++ test_fwk/src/lib.rs
@@ -7,6 +7,12 @@ use ansi_rgb::{cyan_blue, green_cyan, red, Foreground};
 
 use core::panic::PanicInfo;
 
+#[derive(Clone, Copy, PartialEq)]
+pub enum Status {
+    Fail,
+    Success,
+}
+
 pub fn runner(tests: &[&dyn Testable]) {
     println!("{}", "Starting test execution".fg(cyan_blue()));
     tests.iter().for_each(|test| test.run());
@@ -29,6 +35,16 @@ pub fn panic_handler(panic_info: &PanicInfo) -> ! {
 pub fn panic_handler_should_panic(panic_info: &PanicInfo) -> ! {
     println!("{} {:?}", "Expected panic at:".fg(green_cyan()), panic_info);
     arm_semihosting::exit(0);
+}
+
+pub fn finish_with_status(status: Status) -> ! {
+    if status == Status::Success {
+        println!("{}", "Done with test execution".fg(green_cyan()));
+        arm_semihosting::exit(0);
+    } else {
+        println!("{}", "Test failed".fg(red()));
+        arm_semihosting::exit(1);
+    }
 }
 
 pub trait Testable {
