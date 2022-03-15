1fe1cf5e8337eb290b3ec6ab8b6b6245ed556c6c
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2022-03-15 20:16:36 +0100

    Do relocations in userspace process

--- userspace_test/src/main.cpp
+++ userspace_test/src/main.cpp
@@ -1,8 +1,5 @@
-typedef __UINT32_TYPE__ u32;
-typedef __UINT64_TYPE__ u64;
-typedef __SIZE_TYPE__ size_t;
-
-#define CLINKAGE extern "C"
+#include "types.h"
+#include "relocations.h"
 
 size_t strlen(const char *str) {
   if (str == nullptr) {
@@ -32,10 +29,50 @@ void sleep(const u64 time_us) {
   "svc 2" : : "r" (time_us) : "x0");
 }
 
-CLINKAGE __attribute__((section(".init"))) int main() {
+extern "C" u8 _rela_start;
+extern "C" u8 _rela_end;
+
+bool format_hex(u32 number, char *str, u32 max_length) {
+  const u32 mask = 0xF0000000;
+  constexpr static auto NIBBLES_IN_U32 = 8;
+
+  if (max_length < NIBBLES_IN_U32 + 1) {
+    return false;
+  }
+
+  for (u32 i = 0; i < NIBBLES_IN_U32; i++) {
+    const auto value = (number & mask) >> 28;
+    if (value >= 10) {
+      str[i] = 'A' + value - 10;
+    } else {
+      str[i] = '0' + value;
+    }
+    number <<= 4;
+  }
+
+  str[NIBBLES_IN_U32 + 1] = '\0';
+  return true;
+}
+
+// base_addr is passed to us via the OS so that we know where the binary was loaded. This can be used for ASLR.
+CLINKAGE int _start(u64 base_addr) {
+  // After booting we need to apply self-relocations (since this is a pie executable there is no dynamic loader to do
+  // any relocations)
+  const RelaEntry *relocations = (const RelaEntry *) &_rela_start;
+  u64 rela_len_bytes = (u64) & _rela_end - (u64) & _rela_start;
+  apply_relocations(base_addr, relocations, rela_len_bytes);
+
+  char str[16];
+
+  int i = 0;
+  // And now we can start doing work
   while (true) {
     puts("Hi there!");
+    format_hex(i, str, 16);
+    puts(str);
+    i++;
     sleep(1'000'000);
   }
+
   return 0;
 }
