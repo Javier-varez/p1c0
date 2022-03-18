c5d666a25fd3d446ace01eca838eccfbd0bd3f7f
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2022-03-18 21:41:08 +0100

    Initcall priority impl

--- fw/tests/initcall_tests.rs
+++ fw/tests/initcall_tests.rs
@@ -5,10 +5,14 @@
 #![reexport_test_harness_main = "test_main"]
 #![feature(default_alloc_error_handler)]
 
+use core::sync::atomic::{AtomicBool, Ordering};
 use p1c0 as _;
 // needed to link libentry (and _start)
 use p1c0_macros::initcall;
 use test_fwk::Status;
+
+static HIGH_PRIO_RUN: AtomicBool = AtomicBool::new(false);
+static MEDIUM_PRIO_RUN: AtomicBool = AtomicBool::new(false);
 
 #[panic_handler]
 fn panic_handler(panic_info: &core::panic::PanicInfo) -> ! {
@@ -20,11 +24,27 @@ pub extern "C" fn kernel_main() {
     test_fwk::finish_with_status(Status::Fail);
 }
 
+#[initcall(priority = 4)]
+fn test_initcall_with_high_prio() {
+    HIGH_PRIO_RUN.store(true, Ordering::Relaxed);
+}
+
+#[initcall(priority = 3)]
+fn test_initcall_with_medium_prio() {
+    MEDIUM_PRIO_RUN.store(true, Ordering::Relaxed);
+}
+
 #[initcall]
-fn test_init_call() {
+fn test_initcall_with_normal_prio() {
     test_main();
-    test_fwk::finish_with_status(Status::Success);
 }
 
 #[test_case]
-fn dummy_test() {}
+fn check_high_priority_did_run() {
+    assert!(HIGH_PRIO_RUN.load(Ordering::Relaxed));
+}
+
+#[test_case]
+fn check_medium_priority_did_run() {
+    assert!(MEDIUM_PRIO_RUN.load(Ordering::Relaxed));
+}
--- p1c0_macros/src/lib.rs
+++ p1c0_macros/src/lib.rs
@@ -1,10 +1,56 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, Item};
+use syn::{
+    parse_macro_input, AttributeArgs, Item, Lit, Meta, MetaNameValue, NestedMeta, PathSegment,
+};
+
+fn make_error(error_message: &str) -> TokenStream {
+    TokenStream::from(quote! {
+        compile_error!(#error_message);
+    })
+}
 
 #[proc_macro_attribute]
-pub fn initcall(_input: TokenStream, annotated_item: TokenStream) -> TokenStream {
+pub fn initcall(input: TokenStream, annotated_item: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(annotated_item as Item);
+    let attr_ast = parse_macro_input!(input as AttributeArgs);
+
+    const MAX_PRIORITY: u32 = 4;
+    const DEFAULT_PRIORITY: u32 = 0;
+
+    let mut priority: Option<u32> = None;
+    for args in attr_ast {
+        match args {
+            NestedMeta::Meta(Meta::NameValue(MetaNameValue {
+                path,
+                lit: Lit::Int(lit),
+                ..
+            })) => {
+                if path.segments.len() != 1 {
+                    return make_error("Attribute metadata must have the form `priority = 1`");
+                }
+
+                let PathSegment { ident, .. } = path.segments.first().unwrap();
+                if ident == "priority" {
+                    let parsed_priority: u32 = lit
+                        .base10_parse()
+                        .expect("Expected an integer literal for `priority`");
+                    if parsed_priority > MAX_PRIORITY {
+                        let error_code = format!("Priority must be between 0 and {}", MAX_PRIORITY);
+                        return make_error(&error_code);
+                    }
+                    priority = Some(parsed_priority);
+                } else {
+                    return make_error("Only the `priority` attribute is currently supported");
+                }
+            }
+            _ => {
+                return make_error(
+                    "The initcall attribute must be of the form `#[initcall(priority = 1)]`",
+                );
+            }
+        }
+    }
 
     if let Item::Fn(function) = ast {
         let name_ident = function.sig.ident;
@@ -12,10 +58,12 @@ pub fn initcall(_input: TokenStream, annotated_item: TokenStream) -> TokenStream
 
         let func_block = function.block;
 
+        let priority = priority.unwrap_or(DEFAULT_PRIORITY);
+
         TokenStream::from(quote! {
             #[cfg(all(target_arch = "aarch64", target_os = "none"))]
             core::arch::global_asm!(
-                core::concat!(".section .initcall.", #name, ", \"a\""),
+                core::concat!(".section .initcall.prio", #priority, ".", #name, ", \"a\""),
                 core::concat!(".quad ", #name),
                 ".previous"
             );
