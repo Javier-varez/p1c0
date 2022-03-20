a5fb1d7ca02c7c73937d424b11b4f26156426dce
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2022-03-20 19:56:38 +0100

    Add prelude

---
+++ p1c0_kernel/src/prelude.rs
@@ -0,0 +1,5 @@
+pub use crate::collections::{
+    intrusive_list::{IntrusiveItem, IntrusiveList},
+    OwnedMutPtr, OwnedPtr,
+};
+pub use crate::{log_debug, log_error, log_info, log_verbose, log_warning, print, println};
