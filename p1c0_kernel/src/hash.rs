8187f6973978191e0a0d3bbcff2ec879f4327e66
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2022-03-24 19:25:52 +0100

    Implement Hasher based on CRC32C

---
+++ p1c0_kernel/src/hash.rs
@@ -0,0 +1,25 @@
+use crate::crc::Crc32C;
+use core::hash::Hasher;
+
+// This is not a cryptografically safe hasher, but it is easy to implement and works well enough.
+pub struct CrcHasher {
+    crc32c: Crc32C,
+}
+
+impl Default for CrcHasher {
+    fn default() -> Self {
+        CrcHasher {
+            crc32c: Crc32C::new(),
+        }
+    }
+}
+
+impl Hasher for CrcHasher {
+    fn finish(&self) -> u64 {
+        self.crc32c.finish() as u64
+    }
+
+    fn write(&mut self, bytes: &[u8]) {
+        self.crc32c.write(bytes);
+    }
+}
