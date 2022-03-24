b34358da827bd1e2133b89ab2e58f26dc667fbbc
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2022-03-24 19:25:31 +0100

    Implement CRC32C

--- p1c0_kernel/src/crc.rs
+++ p1c0_kernel/src/crc.rs
@@ -55,6 +55,72 @@ pub fn crc16(seed: u16, data: &[u8]) -> u16 {
     crc16
 }
 
+mod crc32c {
+    const POLY: u32 = 0x82F63B78; // CRC32C
+
+    const fn generate_coefficient(byte: u8) -> u32 {
+        let mut value = byte as u32;
+
+        let mut i = 0;
+        while i < 8 {
+            if (0x1 & value) != 0 {
+                value >>= 1;
+                value ^= POLY;
+            } else {
+                value >>= 1;
+            }
+
+            i += 1;
+        }
+
+        value
+    }
+
+    const fn generate_table() -> [u32; 256] {
+        let mut table = [0; 256];
+        let mut i = 0;
+        while i < 256 {
+            table[i] = generate_coefficient(i as u8);
+            i += 1;
+        }
+        table
+    }
+
+    pub(super) static TABLE: [u32; 256] = generate_table();
+}
+
+pub struct Crc32C {
+    current_value: u32,
+}
+
+impl Crc32C {
+    const INITIAL_VALUE: u32 = 0xFFFFFFFF;
+    const XOR_OUT: u32 = 0xFFFFFFFF;
+
+    pub const fn new() -> Self {
+        Self {
+            current_value: Self::INITIAL_VALUE,
+        }
+    }
+
+    pub fn write(&mut self, bytes: &[u8]) {
+        for byte in bytes {
+            let index = *byte ^ (self.current_value & 0xff) as u8;
+            self.current_value = (self.current_value >> 8) ^ crc32c::TABLE[index as usize];
+        }
+    }
+
+    pub fn finish(&self) -> u32 {
+        self.current_value ^ Self::XOR_OUT
+    }
+}
+
+pub fn crc32c(data: &[u8]) -> u32 {
+    let mut crc32c = Crc32C::new();
+    crc32c.write(data);
+    crc32c.finish()
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -82,5 +148,12 @@ mod test {
             ),
             0
         );
+    }
+
+    #[test]
+    fn test_crc32c() {
+        assert_eq!(crc32c(&[0x00]), 0x527D5351);
+        assert_eq!(crc32c(&[0x00, 0x01, 0x02, 0xA5]), 0x5DD948ED);
+        assert_eq!(crc32c(&[0x12, 0x23, 0x4F, 0xFF]), 0xA01D7DB4);
     }
 }
