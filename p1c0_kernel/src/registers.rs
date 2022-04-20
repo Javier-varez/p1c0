d470635248dd5bbcba35698e9fab5d32cd9573bc
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2022-04-20 22:57:42 +0200

    Implement Inaccessible<T>
    This shall be used for register data that shall never be accessed.

--- p1c0_kernel/src/registers.rs
+++ p1c0_kernel/src/registers.rs
@@ -1,3 +1,6 @@
+#[repr(transparent)]
+pub struct Inaccessible<T>(T);
+
 mod sys_impl_apl_ehid4 {
     tock_registers::register_bitfields! { u64,
         pub SYS_IMPL_APL_EHID4 [
@@ -19,6 +22,7 @@ mod sys_impl_apl_ehid4 {
         1
     );
 }
+
 pub use sys_impl_apl_ehid4::SYS_IMPL_APL_EHID4;
 
 mod sys_impl_apl_hid4 {
@@ -42,6 +46,7 @@ mod sys_impl_apl_hid4 {
         0
     );
 }
+
 pub use sys_impl_apl_hid4::SYS_IMPL_APL_HID4;
 
 mod sys_impl_apl_hid5 {
@@ -61,6 +66,7 @@ mod sys_impl_apl_hid5 {
         0
     );
 }
+
 pub use sys_impl_apl_hid5::SYS_IMPL_APL_HID5;
 
 mod sys_impl_apl_ehid9 {
@@ -80,6 +86,7 @@ mod sys_impl_apl_ehid9 {
         1
     );
 }
+
 pub use sys_impl_apl_ehid9::SYS_IMPL_APL_EHID9;
 
 mod sys_impl_apl_ehid10 {
@@ -100,6 +107,7 @@ mod sys_impl_apl_ehid10 {
         1
     );
 }
+
 pub use sys_impl_apl_ehid10::SYS_IMPL_APL_EHID10;
 
 mod sys_impl_apl_ehid20 {
@@ -122,31 +130,37 @@ mod sys_impl_apl_ehid20 {
         2
     );
 }
+
 pub use sys_impl_apl_ehid20::SYS_IMPL_APL_EHID20;
 
 mod s3_4_c15_c5_0 {
     crate::define_register!(S3_4_C15_C5_0, (), 3, 4, 15, 5, 0);
 }
+
 pub use s3_4_c15_c5_0::S3_4_C15_C5_0;
 
 mod sys_impl_apl_amx_ctl_el1 {
     crate::define_register!(SYS_IMPL_APL_AMX_CTL_EL1, (), 3, 4, 15, 1, 4);
 }
+
 pub use sys_impl_apl_amx_ctl_el1::SYS_IMPL_APL_AMX_CTL_EL1;
 
 mod sys_impl_apl_amx_ctl_el2 {
     crate::define_register!(SYS_IMPL_APL_AMX_CTL_EL2, (), 3, 4, 15, 4, 7);
 }
+
 pub use sys_impl_apl_amx_ctl_el2::SYS_IMPL_APL_AMX_CTL_EL2;
 
 mod sys_impl_apl_amx_ctl_el12 {
     crate::define_register!(SYS_IMPL_APL_AMX_CTL_EL12, (), 3, 4, 15, 4, 6);
 }
+
 pub use sys_impl_apl_amx_ctl_el12::SYS_IMPL_APL_AMX_CTL_EL12;
 
 mod s3_4_c15_c10_4 {
     crate::define_register!(S3_4_C15_C10_4, (), 3, 4, 15, 10, 4);
 }
+
 pub use s3_4_c15_c10_4::S3_4_C15_C10_4;
 
 mod sys_impl_apl_cyc_ovrd {
@@ -169,6 +183,7 @@ mod sys_impl_apl_cyc_ovrd {
         0
     );
 }
+
 pub use sys_impl_apl_cyc_ovrd::SYS_IMPL_APL_CYC_OVRD;
 
 mod sys_impl_apl_acc_cfg {
@@ -188,9 +203,11 @@ mod sys_impl_apl_acc_cfg {
         0
     );
 }
+
 pub use sys_impl_apl_acc_cfg::SYS_IMPL_APL_ACC_CFG;
 
 mod sys_impl_apl_pmcr0 {
     crate::define_register!(SYS_IMPL_APL_PMCR0, (), 3, 1, 15, 0, 0);
 }
+
 pub use sys_impl_apl_pmcr0::SYS_IMPL_APL_PMCR0;
